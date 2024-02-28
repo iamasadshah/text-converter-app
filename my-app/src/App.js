e86834c9de1b66f4ab09e92b691723d256eae89a
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-02-28 18:15:43 +0500

    dark mode added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm'
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState(`light`);
   return (
     <>
     
-      <Navbar title="Text Converter"/>
+      <Navbar title="Text Converter" mode={mode}/>
       <div className="container"> 
       <TextForm heading = "Enter Text to Analyze"/>
       </div>
-      
     </>
   )
 }
--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark " id='navbar'>
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} id='navbar'>
   <a className="navbar-brand mx-5" href="/">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -28,5 +28,4 @@ export default function Navbar(props) {
 
 Navbar.prototype = {
     title : PropTypes.string,   /* Set the property of title to string */
-    about : PropTypes.string    /* Set the property of about to string */
 }
