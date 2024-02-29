6cfc930fed5ef6ee694eb5a7b1ba106632d17d53
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-02-29 22:05:27 +0500

    dark mode in navbar

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,37 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
+  // State variable to track whether dark mode is enabled or not
   const [darkMode, setDarkMode] = useState(false);
 
+  // Function to toggle dark mode
   const toggleDarkMode = () => {
+    // Inverting the current state of dark mode when the button is clicked
     setDarkMode(!darkMode);
-    // You can perform additional actions here when switching modes,
-    // such as updating a context or storing the mode in local storage.
+    // Additional actions such as updating context or storing mode in local storage can be performed here
   };
+
   return (
     <div>
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} id='navbar'>
-  <a className="navbar-brand mx-5" href="/">{props.title}</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+      {/* Navbar */}
+      <nav className={`navbar navbar-expand-lg navbar-${darkMode ? 'dark' : 'light'} bg-${darkMode ? 'dark' : 'light'}`} id='navbar'>
+        {/* Navbar Brand */}
+        <a className="navbar-brand mx-5" href="/">{props.title}</a>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      {/* <li className="nav-item">
-        <a className="nav-link" href="/">{props.about}</a>
-      </li> */}
-    </ul>
-    <button className="btn btn-outline-primary mx-2" onClick={toggleDarkMode}>
+        {/* Navbar Toggler */}
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Collapse */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          {/* Navbar Items */}
+          <ul className="navbar-nav mr-auto">
+            {/* Navbar Links */}
+            {/* <li className="nav-item">
+              <a className="nav-link" href="/">{props.about}</a>
+            </li> */}
+          </ul>
+
+          {/* Dark Mode Switch Button */}
+          <button className="btn btn-outline-primary mx-2" onClick={toggleDarkMode}>
             {darkMode ? 'Light Mode' : 'Dark Mode'}
           </button>
-  </div>
-</nav>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-Navbar.prototype = {
-    title : PropTypes.string,   /* Set the property of title to string */
-}
+// PropTypes for validation
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired, // Ensure title is a required string
+};
